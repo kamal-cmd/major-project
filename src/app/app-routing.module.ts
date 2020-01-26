401e683cc4c000516b512932ce0a4889798eb2f3
internnexus <internfimt90@gmail.com>
2020-01-26 12:24:26 +0530

    major project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MiddleComponent } from './middle/middle.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'header',component:HeaderComponent},
+  {path:'middle',component:MiddleComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MiddleComponent } from './middle/middle.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    MiddleComponent,
+    FooterComponent,
+    AboutusComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
